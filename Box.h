d50710ed4b20801cc0e6027817925557aa5d6f9b
IT21377594 <99853557+IT21377594@users.noreply.github.com>
2022-05-08 10:13:18 +0000

    Codes were changed in tutorial 6

--- Box.h
+++ Box.h
@@ -1,11 +1,16 @@
 class Box {
     private:
-       int length;
-       int width;
-       int height;
+      int length;
+      int width;
+      int height;
     public:
-       // write prototypes of setters for length, width and height
-       // write prototypes of getters for length, width and height
+      void setLength(int l);
+      void setWidth(int w);
+      void setHeight(int h);
+
+      int getLength();
+      int getWidth();
+      int getHeight();
  
-       int calcVolume();
+      int calcVolume();
 };
